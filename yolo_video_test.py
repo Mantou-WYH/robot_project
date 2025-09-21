38b751fcd77362a175f1179a32b915a6f5e82645
Mantou-WYH <1259269714@qq.com>
2025-09-22 00:58:31 +0800

    UPG

--- yolo_video_test.py
+++ yolo_video_test.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     ep_robot.initialize(conn_type="ap")
     ep_camera = ep_robot.camera
     model = YOLO("/home/scy/robot_project/yolov5s.pt")
+    model.to('cuda')
     ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
     while True:
         img = ep_camera.read_cv2_image()
