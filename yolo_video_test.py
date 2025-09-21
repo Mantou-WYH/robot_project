af8b84fa3dbe07fba118b54cb798b48c0c85eb2e
Mantou-WYH <1259269714@qq.com>
2025-09-21 23:26:02 +0800

    succeeful deal with one img

--- yolo_video_test.py
+++ yolo_video_test.py
@@ -1,20 +1,21 @@
-from robomaster import robot
-from robomaster import camera
+#from robomaster import robot
+#from robomaster import camera
 from ultralytics import YOLO
-
+import cv2
 
 
 if __name__ == '__main__':
-    ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="ap")
-    ep_camera = ep_robot.camera
+    #ep_robot = robot.Robot()
+    #ep_robot.initialize(conn_type="ap")
+    #ep_camera = ep_robot.camera
     # Load a pretrained YOLO model
-    model = YOLO("yolo5s.pt")
+    model = YOLO("/home/scy/robot_project/yolov5s.pt")
 
     # Perform object detection on an image
-    img = ep_camera.read_cv2_image()
-    results = model(img)
+    #img = ep_camera.read_cv2_image()
+    results = model("/home/scy/robot_project/detal/test1.jpeg")
 
     # Visualize the results
-    for result in results:
-        result.show()
+    img = results[0].plot()
+    cv2.imshow("test",img)
+    cv2.waitKey(3000)
