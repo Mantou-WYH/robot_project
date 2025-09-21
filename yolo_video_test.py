656e7edaeb625137f6ca196abcd8befc2037c16c
Mantou-WYH <1259269714@qq.com>
2025-09-21 23:28:33 +0800

    try of video

--- yolo_video_test.py
+++ yolo_video_test.py
@@ -1,21 +1,21 @@
-#from robomaster import robot
-#from robomaster import camera
+from robomaster import robot
+from robomaster import camera
 from ultralytics import YOLO
 import cv2
 
 
 if __name__ == '__main__':
-    #ep_robot = robot.Robot()
-    #ep_robot.initialize(conn_type="ap")
-    #ep_camera = ep_robot.camera
-    # Load a pretrained YOLO model
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+    ep_camera = ep_robot.camera
     model = YOLO("/home/scy/robot_project/yolov5s.pt")
 
-    # Perform object detection on an image
-    #img = ep_camera.read_cv2_image()
-    results = model("/home/scy/robot_project/detal/test1.jpeg")
+    while True:
+        img = ep_camera.read_cv2_image()
+        results = model(img)
 
-    # Visualize the results
-    img = results[0].plot()
-    cv2.imshow("test",img)
-    cv2.waitKey(3000)
+        # Visualize the results
+        img_result = results[0].plot()
+        cv2.imshow("test",img_result)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
